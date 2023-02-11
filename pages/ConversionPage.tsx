0f98a127d9f9a91cf7b20dc4595b0c022cecce99
Olaniran Micheal <olaniranmicheal2020@gmail.com>
2023-02-11 10:18:18 +0100

    added loader

--- pages/ConversionPage.tsx
+++ pages/ConversionPage.tsx
@@ -4,53 +4,49 @@ import { motion } from 'framer-motion';
 import Image from 'next/image'
 import { Inter } from '@next/font/google';
 import Loader from "@/src/components/Loader"
-
+import SyncLoader from "react-spinners/SyncLoader";
 
 const inter = Inter({ subsets: ['latin'] });
 
 interface Props {}
 
+const override: CSSProperties = {
+  display: "block",
+  margin: "0 auto",
+};
 const ConversionPage: React.FC<Props> = () => {
   
   const [loading, setLoading] = React.useState<boolean>(false);
   const [selectedImage, setSelectedImage] = useState<File | null >(null)
   const [textResult, setTextResult] = useState<string>("");
 
-  const inputRef = useRef<HTMLInputElement>(null);
+    const worker = createWorker();
 
-  const worker = createWorker();
-  const convertImageToText = useCallback(async () => {
-    
-    if (!selectedImage) return;
-    setLoading(true);
-    await (await worker).load();
-    await (await worker).loadLanguage("eng");
-    await (await worker).initialize("eng");
-    
-    const { data } = await (await worker).recognize(selectedImage);
-    setLoading(false);
-    setTextResult(data.text);
-    
-    console.log(data.text);
-  },[worker,selectedImage])
-
-  useEffect(()=>{
-    
-    convertImageToText();
-  },[selectedImage, convertImageToText])
-
-    
-  
-  const changeImageButton = (e: React.ChangeEvent<HTMLInputElement> | null) => {
+    const changeImageButton = (e: React.ChangeEvent<HTMLInputElement> | null) => {
     if (e && e.target.files && e.target.files[0]) {
-      
-      setSelectedImage(e.target.files[0]);
+    setLoading(true)
+    setSelectedImage(e.target.files[0]);
     } else {
-      setSelectedImage(null);
-      setTextResult("");
+    setSelectedImage(null);
+    setTextResult("");
     }
-  }
-  const src =  (selectedImage ? URL.createObjectURL(selectedImage) : '');
+    };
+
+    const convertImageToText = useCallback(async () => {
+      if (!selectedImage) return;
+      
+      await (await worker).load()
+      await (await worker).loadLanguage("eng");
+      await (await worker).initialize("eng");
+      const { data } = await (await worker).recognize(selectedImage);
+      setTextResult(data.text);
+      setLoading(false)
+  }, [worker, selectedImage]);
+    
+  useEffect(() => {
+    convertImageToText();
+  }, [selectedImage, convertImageToText]);
+  const src = selectedImage ? URL.createObjectURL(selectedImage) : '';
 
 
   return (
@@ -74,21 +70,25 @@ const ConversionPage: React.FC<Props> = () => {
       
       {!loading ? (
         <div className='md:mx-[100px] rounded-lg px-[100px] py-12 flex justify-between bg-[#1E1E1E]'>
-        <textarea cols={40}  id='textarea' className={`text-white outline-none rounded-lg px-4 py-4 text-center ${inter.className} bg-[#2B2D2E]`} value={textResult} readOnly={true} />
-        <div className='rounded-lg h-[260px] border-none'>
-          <Image
-            className='bg-[#2B2D2E] object-cover h-full rounded-lg'
-            src={src}
-            alt={''}
-            height={130}
-            width={450}
-          />
-        </div>
+          <textarea cols={40}  id='textarea' className={`text-white scrollbar-thin md:scrollbar scrollbar-track-gray-400/20 scrollbar-thumb-[#28272A]  overflow-hidden outline-none rounded-lg px-9 py-4 ${inter.className} bg-[#2B2D2E]`} value={textResult} readOnly={true} />
+          <div className='rounded-lg h-[260px] border-none'>
+            <Image
+              className='bg-[#2B2D2E] object-cover h-full rounded-lg'
+              src={src}
+              alt={'image'}
+              height={130}
+              width={450}
+            />
+          </div>
       </div>
       ):(
         <div className='md:mx-[100px] rounded-lg px-[100px] py-12 flex justify-between bg-[#1E1E1E]'>
-        <Loader />
-        <Loader />
+         <SyncLoader 
+         cssOverride={override}
+         size={20}
+         aria-label="Loading Spinner"
+         data-testid="loader"
+         color="#1E1E1E" />
       </div>
       )
     }
@@ -96,6 +96,7 @@ const ConversionPage: React.FC<Props> = () => {
     </div>
   );
 }
+
 
 {/* <div className='text-black bg-white h-screen'>
       <h1 className='text-black bg-white'>OCR Text Recognition</h1>
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -7,7 +7,7 @@ export default function App({ Component, pageProps }: AppProps) {
     <div className='h-screen overflow-y-scroll
 scrollbar-thin md:scrollbar
 scrollbar-track-gray-400/20
-scrollbar-thumb-[#F7AB0A]/80 overflow-hidden snap-mandatory snap-y z-0'>
+scrollbar-thumb-[#28272A] overflow-hidden snap-mandatory snap-y z-0'>
       <Header />
       <Component {...pageProps} />
     </div>
