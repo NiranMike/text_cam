9fd8a8f2dd5962e8939fbb35f968a0ddcfa363ee
Olaniran Micheal <olaniranmicheal2020@gmail.com>
2023-02-19 14:22:19 +0100

    fix auth bug

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -1,10 +1,5 @@
 import NextAuth from "next-auth"
-import GoogleProvider from "next-auth/providers/google"
-import {FirestoreAdapter} from "@next-auth/firebase-adapter"
-import { db, app } from "@/firebase";
-import * as firestoreFunctions from "firebase/firestore"
-import { getFirestore } from "firebase/firestore";
-import {useFirebase} from "@/firebase";
+import GoogleProvider from "next-auth/providers/google" 
 
 export default NextAuth({ 
     providers: [
@@ -14,10 +9,6 @@ export default NextAuth({
             clientSecret: process.env.GOOGLE_SECRET
           }
         ),
-    ],
-  adapter: FirestoreAdapter({
-      db: useFirebase().db,
-      ...firestoreFunctions
-    }),
-    // ...
+  ],
+  secret: process.env.JWT_SECRET
 });
